d7e7e66f6bfeefb4268ef16fab23f5802ffc7037
Eilan <eilanlaken@gmail.com>
2024-03-13 18:25:49 -0500

    m

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,22 +11,6 @@ import java.nio.FloatBuffer;
 public class Main {
 
     public static void main(String[] args) {
-        float[] floatArray = {1,2,3,4,5};
-        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(10 * Float.BYTES);
-        byteBuffer.order(ByteOrder.nativeOrder()); // Set the byte order to native (usually little-endian or big-endian)
-        // Create a FloatBuffer view of the ByteBuffer
-        FloatBuffer floatBuffer = byteBuffer.asFloatBuffer();
-        // Copy the float array to the FloatBuffer
-
-        System.out.println(floatBuffer.limit());
-        floatBuffer.put(floatArray,2,2);
-        System.out.println(floatBuffer.position());
-        floatBuffer.flip();
-
-        while (floatBuffer.hasRemaining()) {
-            System.out.println(floatBuffer.get());
-        }
-
         WindowAttributes config = new WindowAttributes();
         Application.createSingleWindowApplication(config);
         Application.launch(new ScreenLoading());
