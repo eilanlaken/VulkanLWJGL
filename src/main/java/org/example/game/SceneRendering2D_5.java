3963c7ccfa11d33a3e011e4cec5ab1afe3df61cb
eilanlaken <49109834+eilanlaken@users.noreply.github.com>
2024-03-13 17:24:58 -0700

    m

--- src/main/java/org/example/game/SceneRendering2D_5.java
+++ src/main/java/org/example/game/SceneRendering2D_5.java
@@ -5,6 +5,7 @@ import org.example.engine.components.ComponentGraphicsCamera;
 import org.example.engine.components.ComponentTransform;
 import org.example.engine.core.assets.AssetUtils;
 import org.example.engine.core.graphics.*;
+import org.example.engine.core.input.Keyboard;
 import org.example.engine.core.memory.MemoryUtils;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
@@ -18,7 +19,10 @@ import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
 // TODO: pretty good solution here:
-//  http://forum.lwjgl.org/index.php?topic=5789.0
+//  TODO: http://forum.lwjgl.org/index.php?topic=5789.0
+// TODO: orphaning - multi buffering:
+// TODO: https://www.cppstories.com/2015/01/persistent-mapped-buffers-in-opengl/#persistence
+// Note: glBufferData invalidates and reallocates the whole buffer. Use glBufferSubData to only update the data inside.
 public class SceneRendering2D_5 extends WindowScreen {
 
     private ShaderProgram shader;
@@ -31,6 +35,8 @@ public class SceneRendering2D_5 extends WindowScreen {
     int BATCH_VERTICES = 8000;
     int VERTEX_SIZE = 5;
 
+    int vbo;
+
     public SceneRendering2D_5() {
 
         final String vertexShaderSrc = AssetUtils.getFileContent("assets/shaders/default-2d-prep.vert");
@@ -42,17 +48,6 @@ public class SceneRendering2D_5 extends WindowScreen {
     @Override
     public void show() {
 
-        float c0 = new Color(0f,1,0,1).toFloatBits();
-        float c1 = new Color(1f,0,0,1).toFloatBits();
-        float c2 = new Color(0f,0,1,0.1f).toFloatBits();
-
-
-        float[] vertices = {
-                -0.5f,0.5f, c0,
-                -0.5f,-0.5f, c1,
-                0.5f,-0.5f, c2,
-                0.5f,0.5f, c1,
-        };
 
         int[] indices = {
                 0,1,3,	//Top left triangle (V0,V1,V3)
@@ -62,11 +57,9 @@ public class SceneRendering2D_5 extends WindowScreen {
         vao = GL30.glGenVertexArrays();
         GL30.glBindVertexArray(vao);
 
-        int vbo = GL15.glGenBuffers();
+        vbo = GL15.glGenBuffers();
         GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo);
-        FloatBuffer interleavedBuffer = BufferUtils.createFloatBuffer(vertices.length);
-        interleavedBuffer.put(vertices).flip();
-        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, interleavedBuffer, GL15.GL_STATIC_DRAW);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, 1024, GL15.GL_DYNAMIC_DRAW);
 
         int vertexSize = 3 * Float.BYTES;
         GL20.glVertexAttribPointer(0, 2, GL11.GL_FLOAT, false, vertexSize, 0);
@@ -89,9 +82,33 @@ public class SceneRendering2D_5 extends WindowScreen {
 
     @Override
     protected void refresh() {
+        // change the fucking buffer
+        float x = 0;
+        float c0 = new Color(0f,1,0,1).toFloatBits();
+        float c1 = new Color(1f,0,0,1).toFloatBits();
+        float c2 = new Color(0f,0,1,0.1f).toFloatBits();
+
+        float[] vertices = {
+                -0.5f,0.5f, c0,
+                -0.5f,-0.5f, c1,
+                0.5f,-0.5f, c2,
+                0.5f,0.5f, c1,
+        };
+
+        float change = Keyboard.isKeyPressed(Keyboard.Key.Q) ? 0.01f : 0;
+
+        floatBuffer.put(-0.5f + change).put(0.5f).put(c0)
+                        .put(-0.5f).put(-0.5f).put(c1)
+                        .put(0.5f).put(-0.5f).put(c2)
+                        .put(0.5f).put(0.5f).put(c1);
+        floatBuffer.flip();
+
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo);
+        GL15.glBufferSubData(GL15.GL_ARRAY_BUFFER, 0, floatBuffer);
+
+
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
         GL11.glClearColor(1,1,1,0);
-
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
