89d1101c158bdc6e96b3dc6171341ccb31c70dea
Eilan <eilanlaken@gmail.com>
2024-03-20 23:23:04 -0500

    m

--- src/main/java/org/example/engine/components/ComponentGraphics2DSpriteStack.java
+++ src/main/java/org/example/engine/components/ComponentGraphics2DSpriteStack.java
@@ -3,7 +3,7 @@ package org.example.engine.components;
 import org.example.engine.core.graphics.Texture;
 
 // TODO: make proper
-public class ComponentGraphics2DSprites {
+public class ComponentGraphics2DSpriteStack {
 
     public Texture texture;
 
