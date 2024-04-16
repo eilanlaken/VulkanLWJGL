a176f6b90133988580b86a37a831350f87ef074d
eilanlaken <49109834+eilanlaken@users.noreply.github.com>
2024-04-15 23:58:38 -0700

    started testing on basic Shape2D functionality. I code better when I'm drunk and with Alyson

--- src/test/java/org/example/engine/core/shape/Shape2DTest.java
+++ src/test/java/org/example/engine/core/shape/Shape2DTest.java
@@ -1,92 +1,125 @@
 package org.example.engine.core.shape;
 
+import org.example.engine.core.math.MathUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Shape2DTest {
 
-    @Test
-    void contains() {
+    private static Shape2D shape;
+
+    @BeforeEach
+    private void setup() {
+        shape = new Shape2D() {
+            @Override
+            public boolean contains(float x, float y) {
+                return false;
+            }
+
+            @Override
+            protected void updateWorldCoordinates() {
+
+            }
+
+            @Override
+            protected float getUnscaledArea() {
+                return 0;
+            }
+
+            @Override
+            protected float getUnscaledBoundingRadius() {
+                return 0;
+            }
+        };
     }
 
     @Test
-    void setTranslation() {
+    void testInit() {
+        Assertions.assertEquals(0.0, shape.x(),      MathUtils.FLOAT_ROUNDING_ERROR);
+        Assertions.assertEquals(0.0, shape.y(),      MathUtils.FLOAT_ROUNDING_ERROR);
+        Assertions.assertEquals(0.0, shape.angle(),  MathUtils.FLOAT_ROUNDING_ERROR);
+        Assertions.assertEquals(1.0, shape.scaleX(), MathUtils.FLOAT_ROUNDING_ERROR);
+        Assertions.assertEquals(1.0, shape.scaleY(), MathUtils.FLOAT_ROUNDING_ERROR);
     }
 
     @Test
-    void setRotation() {
+    void dx() {
+        shape.dx(0.0f);
+        Assertions.assertEquals(0.0, shape.x(), MathUtils.FLOAT_ROUNDING_ERROR);
+        Assertions.assertEquals(0.0, shape.y(), MathUtils.FLOAT_ROUNDING_ERROR);
+
+        shape.dx(30.0f);
+        shape.dx(15.0f);
+        Assertions.assertEquals(45.0, shape.x(), MathUtils.FLOAT_ROUNDING_ERROR);
+
     }
 
     @Test
-    void setScale() {
+    void dy() {
     }
 
     @Test
-    void resetTransform() {
+    void dx_dy_rot() {
+
     }
 
     @Test
-    void setTransform() {
+    void rot() {
+
     }
 
     @Test
-    void getArea() {
+    void x() {
     }
 
     @Test
-    void getBoundingRadius() {
+    void y() {
     }
 
     @Test
-    void update() {
+    void xy() {
     }
 
     @Test
-    void testUpdate() {
+    void angle() {
     }
 
     @Test
-    void applyCurrentTransformToLocalCoordinates() {
+    void scaleX() {
     }
 
     @Test
-    void getX() {
+    void scaleY() {
     }
 
     @Test
-    void getY() {
+    void scaleXY() {
     }
 
     @Test
-    void getAngle() {
+    void transform() {
     }
 
     @Test
-    void getScaleX() {
+    void testX() {
     }
 
     @Test
-    void getScaleY() {
+    void testY() {
     }
 
     @Test
-    void testContains() {
+    void testAngle() {
     }
 
     @Test
-    void bakeCurrentTransformToLocalCoordinates() {
+    void testScaleX() {
     }
 
     @Test
-    void updateWorldCoordinates() {
-    }
-
-    @Test
-    void calculateOriginalArea() {
-    }
-
-    @Test
-    void calculateOriginalBoundingRadius() {
+    void testScaleY() {
     }
 }
