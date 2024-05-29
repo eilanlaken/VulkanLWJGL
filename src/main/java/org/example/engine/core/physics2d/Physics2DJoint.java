0489be9e58d0cc3440de019f99b1d0638c03e181
eilanlaken <49109834+eilanlaken@users.noreply.github.com>
2024-05-29 15:47:02 -0700

    m

--- src/main/java/org/example/engine/core/physics2d/Physics2DJoint.java
+++ src/main/java/org/example/engine/core/physics2d/Physics2DJoint.java
@@ -1,5 +1,7 @@
 package org.example.engine.core.physics2d;
 
+import org.example.engine.core.math.MathVector2;
+
 public abstract class Physics2DJoint {
 
     public Physics2DBody body_a;
@@ -8,6 +10,37 @@ public abstract class Physics2DJoint {
     Physics2DJoint(Physics2DBody body_a, Physics2DBody body_b) {
         this.body_a = body_a;
         this.body_b = body_b;
+    }
+
+    public abstract void  getReactionForce (float inv_dt, MathVector2 out);
+    public abstract float getReactionTorque(float inv_dt);
+
+    public abstract void    initVelocityConstraints (SolverData data);
+    public abstract void    solveVelocityConstraints(SolverData data);
+    public abstract boolean solvePositionConstraints(SolverData data);
+
+    public class SolverData {
+        public TimeStep step;
+        public MathVector2[] positions;
+        public float[] angles;
+        public MathVector2[] velocities;
+        public float[] omegas;
+    }
+
+    public class TimeStep {
+
+        public float dt;
+
+        public float inv_dt; //inverse time step (0 if dt == 0).
+
+        /** dt * inv_dt0 */
+        public float dtRatio;
+
+        public int velocityIterations;
+
+        public int positionIterations;
+
+        public boolean warmStarting;
     }
 
 }
--- src/main/java/org/example/engine/core/physics2d/Physics2DJointSpring.java
+++ src/main/java/org/example/engine/core/physics2d/Physics2DJointSpring.java
@@ -1,5 +1,7 @@
 package org.example.engine.core.physics2d;
 
+import org.example.engine.core.math.MathVector2;
+
 public class Physics2DJointSpring extends Physics2DJoint {
 
     public float k;
@@ -11,4 +13,28 @@ public class Physics2DJointSpring extends Physics2DJoint {
         this.l = l;
     }
 
+    @Override
+    public void getReactionForce(float inv_dt, MathVector2 out) {
+
+    }
+
+    @Override
+    public float getReactionTorque(float inv_dt) {
+        return 0;
+    }
+
+    @Override
+    public void initVelocityConstraints(SolverData data) {
+
+    }
+
+    @Override
+    public void solveVelocityConstraints(SolverData data) {
+
+    }
+
+    @Override
+    public boolean solvePositionConstraints(SolverData data) {
+        return false;
+    }
 }
