c1efbc6a2caf5dfb4de9bff07cba113f8cb92f1c
eilanlaken <49109834+eilanlaken@users.noreply.github.com>
2024-07-02 06:14:16 -0700

    nothing

---
+++ src/main/java/org/example/engine/core/graphics/TextureGenerator.java
@@ -0,0 +1,26 @@
+package org.example.engine.core.graphics;
+
+// TODO: implement
+public final class TextureGenerator {
+
+    private TextureGenerator() {}
+
+    /* Noise */
+    public static void generateTextureNoisePerlin() {}
+    public static void generateTextureNoiseSimplex() {}
+    public static void generateTextureNoiseWhite() {}
+    public static void generateTextureNoiseValue() {}
+    public static void generateTextureNoiseVornoi() {}
+
+    /* Patterns */
+    public static void generateTextureCheckers() {}
+    public static void generateTextureGradient() {}
+    public static void generateTextureWave() {}
+
+    /* Maps */
+    public static void generateTextureMapNormal() {}
+    public static void generateTextureMapRoughness() {}
+    public static void generateTextureMapMetallic() {}
+
+
+}
